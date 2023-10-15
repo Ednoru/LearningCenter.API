f8a6d7cf3c55252c835c15c36e94fac3d34c1226
Eduardo Chero <eduardochero08@gmail.com>
2023-10-14 19:48:02 -0500

    feat(categories): created Category Model

---
+++ LearningCenter.API/Learning/Domain/Models/Category.cs
@@ -0,0 +1,10 @@
+namespace Learning_Center_API.Learning.Domain.Models;
+
+public class Category
+{
+    // Patron de diseño: Identity Field
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    
+}
